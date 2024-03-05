70c8641304df8b13bfda965e2364a85948dce63b
riizukilight <71415993+riizukilight@users.noreply.github.com>
2024-03-05 15:24:37 +0700

    Delete ICA_filtering.py

--- ICA_filtering.py
+++
@@ -1,6 +0,0 @@
-from sklearn.decomposition import FastICA
-def apply_ica(eeg_data, num_components):
-    ica = FastICA(n_components=num_components)
-    components = ica.fit_transform(eeg_data.T).T
-    return components
-#FAST ICA DECOMPOSITION
